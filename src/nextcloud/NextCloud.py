2e3b37319a0a8db5e47921008e658b3910150f3f
David Berthel <gitlab@davidberthel.de>
2020-05-24 09:28:09 +0000

    Added Deck API as functionality class

--- src/nextcloud/NextCloud.py
+++ src/nextcloud/NextCloud.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from .requester import OCSRequester, WebDAVRequester
-from .api_wrappers import OCS_API_CLASSES, WEBDAV_CLASS
-
+from .requester import OCSRequester, WebDAVRequester, DeckRequester
+from .api_wrappers import OCS_API_CLASSES, WEBDAV_CLASS, DECK_CLASS
 
 class NextCloud(object):
 
@@ -11,9 +10,11 @@ class NextCloud(object):
 
         ocs_requester = OCSRequester(endpoint, user, password, json_output)
         webdav_requester = WebDAVRequester(endpoint, user, password)
+        deck_requester = DeckRequester(endpoint, user, password, json_output)
 
         self.functionality_classes = [api_class(ocs_requester) for api_class in OCS_API_CLASSES]
         self.functionality_classes.append(WEBDAV_CLASS(webdav_requester, json_output=json_output))
+        self.functionality_classes.append(DECK_CLASS(deck_requester))
 
         for functionality_class in self.functionality_classes:
             for potential_method in dir(functionality_class):
